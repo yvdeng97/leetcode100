6ea28ff2c88e6511e17bd9388be0034228c54e48
yvdeng97 <yvdeng97@gmail.com>
2026-03-08 23:16:19 +0800

    Add files via upload

---
+++ ReturnTwoSum.java
@@ -0,0 +1,54 @@
+package day01;
+
+import java.util.*;
+
+public class ReturnTwoSum {
+    public static void main(String[] args) {
+        //给定一个整数数组 nums 和一个整数目标值target，请你在该数组中找出和为目标值target的那两个整数，并返回它们的数组下标。
+        //你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+        //你可以按任意顺序返回答案。
+        //------------------------------------------------------------------------------------------------------------------
+        Scanner data = new Scanner(System.in);
+
+        //定义一个动态大小的数组用来接收输入的数据
+        List<Integer> list = new ArrayList<>();
+        System.out.println("请输入数组元素(输入-99退出)：");
+        while (true) {
+            int num = data.nextInt(); // 先读到一个变量里
+            if (num == -99) {
+                System.out.println("输入结束");
+                break;
+            }
+            list.add(num); // 再加入列表
+        }
+
+        // 2. 将 List 转换为原生 int[] 数组 (解决类型不匹配问题)
+        int[] nums = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            nums[i] = list.get(i);
+        }
+
+        System.out.println("请输入目标值：");
+        int target = data.nextInt();
+
+        //调用twoSum方法，并打印返回的数组
+        System.out.println(Arrays.toString(new ReturnTwoSum().twoSum(nums, target)));
+
+        data.close();
+    }
+
+    //创建一个方法，接收一个数组和一个目标值，返回结果
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return null;
+    }
+
+}
